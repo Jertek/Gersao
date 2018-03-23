c3d87272dfdb029680cfc65aaf278856179885ac
Jertek <tulioarki@gmail.com>
2018-03-22 23:51:39 -0300

    Forms

---
+++ Scripts/Frames/Projeto Frames_files/arquivo.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $idade = $_POST['idade'];
+    $sexo = $_POST['sexo'];
+    $msg = $_POST['msg'];
+    $estuda = $_POST['estuda'];
+
+    echo "Nome = $nome </br>";
+    echo "Email = $email </br>";
+    echo "Idade = $idade </br>";
+    echo "Mensagem = $msg </br>";
+    echo "Gostaria de estudar na Fatec? = $estuda </br>";
+
+
+?>
